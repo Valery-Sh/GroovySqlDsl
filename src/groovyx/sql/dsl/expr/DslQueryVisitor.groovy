511f9508a390049094e6eb7711da283a1fe12bc7
Valery Shyshkin <vns.shyshkin@gmail.com>
2011-07-24 11:18:02 -0700

    Start push 999

--- src/groovyx/sql/dsl/expr/DslQueryVisitor.groovy
+++ src/groovyx/sql/dsl/expr/DslQueryVisitor.groovy
@@ -52,22 +52,20 @@ class DslQueryVisitor extends ClassCodeVisitorSupport {
     def rootExpression
     
     def methods = []
-    def supportedMethods = ["from","join","leftJoin","rightJoin","fullJoin"]
+    def supportedMethods = ["from","join","leftjoin",
+                            "rightjoin","fulljoin",
+                            "on", "where","groupby",
+                            "having","orderby"]
     
-/*    def DslQueryVisitor(sourceUnit,state) {
-        this.sourceUnit = sourceUnit
-        this.state = state
-    }
-*/    
     public void visitClosureExpression(ClosureExpression expression) {
         expression.getCode().visit(this);
         //println "CLOSURE: " + expression.code.class
     }
     public void visitBlockStatement(BlockStatement block) {
-/*        for (Statement statement : block.getStatements()) {
-            statement.visit(this);
+        /*        for (Statement statement : block.getStatements()) {
+        statement.visit(this);
         }
-*/        
+         */        
         def msg = "Unsupported expression. Must be method call."
         block.statements.each() {
             if ( ! (it instanceof ExpressionStatement && it.expression instanceof MethodCallExpression) ) {
@@ -76,65 +74,136 @@ class DslQueryVisitor extends ClassCodeVisitorSupport {
             }
             //methods << it.expression.method.text
             
+            def list = []
+            
             def object = it.expression
-            def cond = true
-            while (cond) {
-                if ( object instanceof MethodCallExpression ) {
-                    methods << object.method.text
+            
+            while (! (object instanceof VariableExpression && "this" == object.text)) {
+                if ( object instanceof MethodCallExpression && object.method.text.toLowerCase() in supportedMethods ) {
+                    list << object
                     object = object.objectExpression
+                } else if ( object instanceof MethodCallExpression) {
+                    msg = "Unsupported method name '${object.method.text}'"
+                    addError(msg,object)
+                    return
                 } else {
                     addError(msg,object)
                     return
                 }
-                cond = ! (object instanceof VariableExpression && "this" == object.text) 
             }//while
+            //methods << list.reverse()
+            methods += list.reverse()
         }//each
+
         methods.each {
-            println it
+            println it.method.text
         }
         
-        methods = methods.reverse()
-   /*     methods.each {
-            if ( ! (object.method.text.toLowerCase() in supportedMethods) )  {
-                addError("Unsupported method name '${object.method.text}'.",object.method)
-                return
-            }
-            
+        if ( !validateMethodOrder() ) {
+            return
         }
-*/        
-
-        
-
     }
     
+    def validateMethodOrder() {
+
+        def i = 0
+        while( i < methods.size() && methods[i].method.text.toLowerCase() == "from" ) {
+            i++
+        }
+        if ( i == 0) {
+            addError("A query must have a method 'from' ",methods[0])
+            return false
+            
+        } else if ( i > 1 ) {
+            addError("Too many methods with a name 'from' ",methods[i-1])
+            return false
+            
+        }
+
+        while( i < methods.size() && methods[i].method.text.toLowerCase().endsWith("join") ) {
+            def j = ++i
+            while ( j < methods.size() && methods[j].method.text.toLowerCase() == "on") {
+                j++
+            }
+            if ( j - i > 1 ) {
+                addError("Too many methods with a name 'on' ",methods[i])
+                return false
+            }
+            i = j
+        }
+        
+        def c = 0
+        while( i < methods.size() && methods[i].method.text.toLowerCase() == "where" ) {
+            i++
+            c++
+        }
+        if ( c > 1 ) {
+            addError("Too many methods with a name 'where' ",methods[i-1])
+            return false
+        }
+        c = 0
+        while( i < methods.size() && methods[i].method.text.toLowerCase() == "groupby" ) {
+            i++
+            c++
+        }
+        if ( c > 1 ) {
+            addError("Too many methods with a name 'groupBy' ",methods[i-1])
+            return false
+        }
+        c = 0
+        while( i < methods.size() && methods[i].method.text.toLowerCase() == "having" ) {
+            i++
+            c++
+        }
+        if ( c > 1 ) {
+            addError("Too many methods with a name 'having' ",methods[i-1])
+            return false
+        }
+        c = 0
+        while( i < methods.size() && methods[i].method.text.toLowerCase() == "orderby" ) {
+            i++
+            c++
+        }
+        if ( c > 1 ) {
+            addError("Too many methods with a name 'orderBy' ",methods[i-1])
+            return false
+        }
+        
+        if ( i < methods.size() ) {
+            addError("Unexpected method with a name '${methods[i].method.text}' ",methods[i])
+            return false
+            
+        }
+        
+    }
     def addError(msg,expr) {
         groovyx.sql.dsl.SqlQueryASTTransformation.addError(sourceUnit,msg,expr)
     }
             
-/*    void visitMethodCallExpression(MethodCallExpression expr) {
-        //println "visitMethodCallExpression.method="  + expr.method.text  
+    /*    void visitMethodCallExpression(MethodCallExpression expr) {
+    //println "visitMethodCallExpression.method="  + expr.method.text  
 
-        expr.objectExpression.visit(this)
-        //def obj = whereList[whereList.size()-1]
-        def obj = whereList.pop()
-        expr.arguments.visit(this)
+    expr.objectExpression.visit(this)
+    //def obj = whereList[whereList.size()-1]
+    def obj = whereList.pop()
+    expr.arguments.visit(this)
                 
-        //def args = whereList[whereList.size()-1]
-        def args = whereList.pop()
+    //def args = whereList[whereList.size()-1]
+    def args = whereList.pop()
                 
-        def newExpr = new ConstructorCallExpression(
-            ClassHelper.make(SqlMethodCallExpression),
-            new TupleExpression(new NamedArgumentListExpression([
-                        new MapEntryExpression(new ConstantExpression('object'), obj),                        
-                        new MapEntryExpression(new ConstantExpression('method'), new ConstantExpression(expr.method.text)),                        
-                        new MapEntryExpression(new ConstantExpression('arguments'), args)                        
+    def newExpr = new ConstructorCallExpression(
+    ClassHelper.make(SqlMethodCallExpression),
+    new TupleExpression(new NamedArgumentListExpression([
+    new MapEntryExpression(new ConstantExpression('object'), obj),                        
+    new MapEntryExpression(new ConstantExpression('method'), new ConstantExpression(expr.method.text)),                        
+    new MapEntryExpression(new ConstantExpression('arguments'), args)                        
                 
-                    ]) ) )
-        whereList << newExpr
-        rootExpression = newExpr
+    ]) ) )
+    whereList << newExpr
+    rootExpression = newExpr
 
     }    
-  */          
+     */          
             
     protected SourceUnit getSourceUnit() { sourceUnit }
     
